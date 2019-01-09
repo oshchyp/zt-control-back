4835b28446450a66a451bd43e2a366c51a90abd9
oshchyp <oshchyp.denys@gmail.com>
2019-01-09 14:52:04 +0200

    zlata elevators. Changing the process of seving the firm

--- models/firms/FirmsByElevatorsAsSave.php
+++ models/firms/FirmsByElevatorsAsSave.php
@@ -10,17 +10,37 @@ namespace app\models\firms;
 
 
 use app\components\models\ModelAsResourceInterface;
+use app\models\ZlataElevators;
 
 class FirmsByElevatorsAsSave extends FirmsAsSave implements ModelAsResourceInterface
 {
 
     use FirmsByElevatorsTrait;
 
+    public $elevatorID;
+
     public function rules()
     {
-        return array_merge(parent::rules(),[['elevatorBit','elevatorBitValidate']]);
+        return array_merge(parent::rules(),[
+            ['elevatorID', 'elevatorIDValidate'],
+            ['elevatorBit','elevatorBitValidate'],
+
+            ]);
     }
 
+
+    public function elevatorIDValidate(){
+        $elevatorObject = ZlataElevators::find()->where(['id'=>$this->elevatorID])->one();
+        if ($elevatorObject){
+            $this->setAttribute('elevatorBit',$elevatorObject->bit);
+        } else {
+            $this->addError('elevatorBit', 'elevator dose not exist!');
+        }
+    }
+
+    /**
+     * @return bool
+     */
     public function elevatorBitValidate(){
         $elevatorBitUser = \Yii::$app->user->identity ? \Yii::$app->user->identity->elevatorBit : 1;
         if ($elevatorBitUser & $this->elevatorBit){
